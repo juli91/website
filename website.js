14f0256857592282882b9ae4e7b03f48dd6f3918
Juli <jwendt91@web.de>
2020-08-01 18:13:22 +0200

    formular absenden

--- website.js
+++ website.js
@@ -6,21 +6,28 @@ const fieldnames = [
     'message',
 ];
 
-document.querySelector('#contact-form').addEventListener('submit', function (e) {
-e.preventDefault();
-// alert("Danke für deine Nachricht! Ich melde mich in der Regel innerhalb von 24 Stunden.");
-const action = document.getElementById('contact-form').getAttribute('action');
-console.log(action)
-fieldnames.forEach(function (fieldname) {
-    const field = document.getElementById(fieldname)
-    console.log(fieldname, field.value)
+document.querySelector('#contact-form').addEventListener('submit', async function (e) {
+    e.preventDefault();
+    let data = '';
+    // alert("Danke für deine Nachricht! Ich melde mich in der Regel innerhalb von 24 Stunden.");
+    const action = document.getElementById('contact-form').getAttribute('action');
+    console.log(action)
+    fieldnames.forEach(function (fieldname, index) {
+        const field = document.getElementById(fieldname)
+        data += fieldname + '=' + field.value;
+        if (index < fieldnames.length -1) {
+            data += '&';
+        }
+        console.log(fieldname, field.value);
 
-})
+    })
+    const response = await fetch (action, {method: 'POST', body: data});
+    console.log(response)
 })
 
 // $("#my-form").submit(function(e) {
 //     e.preventDefault();
-  
+
 //     var $form = $(this);
 //     $.post($form.attr("action"), $form.serialize()).then(function() {
 //       alert("Thank you!");
