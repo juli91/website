5ade2c5c7c275f9f12ec4e9d85b7a5a8287d73b1
Juli <jwendt91@web.de>
2020-08-01 17:39:04 +0200

    formularanpassung

--- website.js
+++ website.js
@@ -1,3 +1,28 @@
+const fieldnames = [
+    'firstName',
+    'surname',
+    'email',
+    'phone',
+    'message',
+];
+
 document.querySelector('#contact-form').addEventListener('submit', function (e) {
-e.preventDefault()
+e.preventDefault();
+// alert("Danke für deine Nachricht! Ich melde mich in der Regel innerhalb von 24 Stunden.");
+const action = document.getElementById('contact-form').getAttribute('action');
+console.log(action)
+fieldnames.forEach(function (fieldname) {
+    const field = document.getElementById(fieldname)
+    console.log(fieldname, field.value)
+
 })
+})
+
+// $("#my-form").submit(function(e) {
+//     e.preventDefault();
+  
+//     var $form = $(this);
+//     $.post($form.attr("action"), $form.serialize()).then(function() {
+//       alert("Thank you!");
+//     });
+//   });
